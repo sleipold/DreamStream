871038fcc051d8026b8320da940a632a698a6bb1
sleipold <simon.leipold@gmx.de>
2019-11-01 02:27:15 +0100

    #1 Welcome Screen - added app name, welcome message and buttons for start and settings.

--- app/src/main/java/com/example/dreamstream/MainActivity.kt
+++ app/src/main/java/com/example/dreamstream/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.dreamstream
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Adding menu to activity
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // onClick listener for menu
+        when(item.itemId) {
+            R.id.settings -> {
+                println("settings got clicked")
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
