ea1b03172435870654f98175c0032179f2fbdb98
sleipold <simon.leipold@gmx.de>
2019-11-19 15:53:39 +0100

    #4 Receiver Connection Screen - added QR-code generator

--- app/src/main/java/com/sleipold/dreamstream/ReceiverConnection.kt
+++ app/src/main/java/com/sleipold/dreamstream/ReceiverConnection.kt
@@ -5,12 +5,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidmads.library.qrgenearator.QRGContents
+import androidmads.library.qrgenearator.QRGEncoder
+import com.google.zxing.WriterException
+import android.R.attr.bitmap
+import android.graphics.Point
+import android.view.WindowManager
+import android.widget.ImageView
+
 
 class ReceiverConnection : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver_connection)
+
+        var inputValue = "foobar42"
+        if (inputValue.isNotEmpty()) {
+            var qrImage: ImageView = findViewById(R.id.ivQrImage)
+            val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
+            val display = windowManager.defaultDisplay
+            val point = Point()
+            display.getSize(point)
+            val width = point.x
+            val height = point.y
+            var smallerDimension = if (width < height) width else height
+            smallerDimension = smallerDimension * 3 / 4
+
+            var qrgEncoder = QRGEncoder(
+                inputValue, null,
+                QRGContents.Type.TEXT,
+                smallerDimension
+            )
+            try {
+                var bitmap = qrgEncoder.encodeAsBitmap()
+                qrImage.setImageBitmap(bitmap)
+            } catch (e: WriterException) {
+                println(e.toString())
+            }
+
+        } else {
+            println("inputValue must not be empty")
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
