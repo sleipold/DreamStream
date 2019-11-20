836623268cccffedcec24355c453f2797c331143
sleipold <simon.leipold@gmx.de>
2019-11-20 17:06:56 +0100

    #5 Sender Screen - devices connecting to each other via Nearby Connections. Basic output to display connection status

--- app/src/main/java/com/sleipold/dreamstream/ReceiverConnection.kt
+++ app/src/main/java/com/sleipold/dreamstream/ReceiverConnection.kt
@@ -8,19 +8,77 @@ import android.view.MenuItem
 import androidmads.library.qrgenearator.QRGContents
 import androidmads.library.qrgenearator.QRGEncoder
 import com.google.zxing.WriterException
-import android.R.attr.bitmap
 import android.graphics.Point
 import android.view.WindowManager
 import android.widget.ImageView
-
+import android.widget.TextView
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.nearby.connection.PayloadTransferUpdate
+import com.google.android.gms.nearby.connection.Payload
+import com.google.android.gms.nearby.connection.PayloadCallback
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.R.attr.name
 
 class ReceiverConnection : AppCompatActivity() {
+    private lateinit var connectionsClient: ConnectionsClient
+    private lateinit var statusText: TextView
+
+    private val inputValue = "foobar42"
+    private val endpointName = "receiver"
+    private val advertisingOptions = AdvertisingOptions.Builder().setStrategy(Strategy.P2P_POINT_TO_POINT).build()
+
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            // A new payload is being sent over.
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Payload progress has updated.
+        }
+    }
+
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            // Automatically accept the connection on both sides.
+            connectionsClient.acceptConnection(endpointId, payloadCallback)
+        }
+
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            when (result.status.statusCode) {
+                ConnectionsStatusCodes.STATUS_OK -> {
+                    // We're connected! Can now start sending and receiving data.
+                    statusText.text = getString(R.string.status_connected_receiver)
+                }
+                ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED -> {
+                    // The connection was rejected by one or both sides.
+                    statusText.text = getString(R.string.status_rejected_receiver)
+                }
+                else -> {
+                    // The connection was broken before it was accepted.
+                    statusText.text = getString(R.string.status_fail_receiver)
+                }
+            }
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            // We've been disconnected from this endpoint. No more data can be
+            // sent or received.
+            statusText.text = getString(R.string.status_disconnect_receiver)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_receiver_connection)
 
-        var inputValue = "foobar42"
+        statusText = findViewById(R.id.txtStatus)
+
+        connectionsClient = Nearby.getConnectionsClient(this)
+
         if (inputValue.isNotEmpty()) {
             var qrImage: ImageView = findViewById(R.id.ivQrImage)
             val windowManager = getSystemService(WINDOW_SERVICE) as WindowManager
@@ -38,15 +96,33 @@ class ReceiverConnection : AppCompatActivity() {
                 smallerDimension
             )
             try {
+                // generating QR code and displaying it
                 var bitmap = qrgEncoder.encodeAsBitmap()
                 qrImage.setImageBitmap(bitmap)
+
+                // starting to advertise to find sender
+                findSender()
             } catch (e: WriterException) {
                 println(e.toString())
             }
-
         } else {
             println("inputValue must not be empty")
         }
+    }
+
+    /** Starts advertising for other device using Nearby Connections.  */
+    private fun startAdvertising() {
+        connectionsClient.startAdvertising(
+                endpointName,
+                packageName,
+                connectionLifecycleCallback,
+                advertisingOptions
+            )
+    }
+
+    private fun findSender() {
+        startAdvertising()
+        statusText.text = getString(R.string.status_searching_sender)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -57,7 +133,7 @@ class ReceiverConnection : AppCompatActivity() {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         // onClick listener for menu
-        when(item.itemId) {
+        when (item.itemId) {
             R.id.home -> {
                 // home icon got clicked -> open welcome activity
                 val homeIntent = Intent(this, Welcome::class.java)
--- app/src/main/java/com/sleipold/dreamstream/Sender.kt
+++ app/src/main/java/com/sleipold/dreamstream/Sender.kt
@@ -3,12 +3,81 @@ package com.sleipold.dreamstream
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import com.google.android.gms.nearby.Nearby
+import com.google.android.gms.nearby.connection.*
+import com.google.android.gms.nearby.connection.DiscoveryOptions
+import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo
+import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback
+import com.google.android.gms.nearby.connection.ConnectionResolution
+import com.google.android.gms.nearby.connection.ConnectionInfo
+import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback
 
 class Sender : AppCompatActivity() {
+
+    private lateinit var receiverName: String
+    private lateinit var opponentEndpointId: String
+
+    private lateinit var connectionsClient: ConnectionsClient
+    private lateinit var statusText: TextView
+
+    private val endpointName = "sender"
+    private val discoveryOptions =
+        DiscoveryOptions.Builder().setStrategy(Strategy.P2P_POINT_TO_POINT).build()
+
+    private val payloadCallback = object : PayloadCallback() {
+        override fun onPayloadReceived(endpointId: String, payload: Payload) {
+            // A new payload is being sent over.
+        }
+
+        override fun onPayloadTransferUpdate(endpointId: String, update: PayloadTransferUpdate) {
+            // Payload progress has updated.
+        }
+    }
+
+    // Callbacks for finding other devices
+    private val endpointDiscoveryCallback = object : EndpointDiscoveryCallback() {
+        override fun onEndpointFound(endpointId: String, info: DiscoveredEndpointInfo) {
+            println("onEndpointFound: endpoint found, connecting")
+            connectionsClient.requestConnection(endpointName, endpointId, connectionLifecycleCallback)
+        }
+
+        override fun onEndpointLost(endpointId: String) {}
+    }
+
+    // Callbacks for connections to other devices
+    private val connectionLifecycleCallback = object : ConnectionLifecycleCallback() {
+        override fun onConnectionInitiated(endpointId: String, connectionInfo: ConnectionInfo) {
+            println("onConnectionInitiated: accepting connection")
+            connectionsClient.acceptConnection(endpointId, payloadCallback)
+            receiverName = connectionInfo.endpointName
+        }
+
+        override fun onConnectionResult(endpointId: String, result: ConnectionResolution) {
+            if (result.status.isSuccess) {
+                println("onConnectionResult: connection successful")
+
+                connectionsClient.stopDiscovery()
+                connectionsClient.stopAdvertising()
+
+                opponentEndpointId = endpointId
+                statusText.text = getString(R.string.status_connected_sender)
+            } else {
+                println("onConnectionResult: sender failed to connect")
+            }
+        }
+
+        override fun onDisconnected(endpointId: String) {
+            println("onDisconnected: disconnected from receiver")
+            statusText.text = getString(R.string.status_disconnect_sender)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sender)
+
+        // Get status text view from layout
+        statusText = findViewById(R.id.txtStatus)
 
         // Get the Intent that started this activity and extract the string
         val connectionSecret = intent.getStringExtra(CONNECTION_SECRET)
@@ -17,6 +86,26 @@ class Sender : AppCompatActivity() {
         findViewById<TextView>(R.id.txtConnectionSecret).apply {
             text = connectionSecret
         }
+
+        connectionsClient = Nearby.getConnectionsClient(this)
+
+        // nearby discovery starting - searching for receiver
+        findReceiver()
     }
+
+    private fun findReceiver() {
+        startDiscovery()
+        statusText.text = getString(R.string.status_searching_receiver)
+    }
+
+    /** Starts looking for other device using Nearby Connections.  */
+    private fun startDiscovery() {
+        connectionsClient.startDiscovery(
+            packageName,
+            endpointDiscoveryCallback,
+            discoveryOptions
+        )
+    }
+
 
 }
